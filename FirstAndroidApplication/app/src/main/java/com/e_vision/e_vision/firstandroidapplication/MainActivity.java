995faad40e4939954c2279df05c8f033ed4860b4
MD RASHEDUL ALAM <smrashedcse@gmail.com>
2017-04-09 11:17:45 +0600

    Toast
    Toast in Android

---
+++ FirstAndroidApplication/app/src/main/java/com/e_vision/e_vision/firstandroidapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.e_vision.e_vision.firstandroidapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(),"Hello Rashed!",Toast.LENGTH_LONG).show();
+    }
+}
